3eb08ef74beb42b6d05cb90a41c86b85340708da
Lee <1097207529@qq.com>
2023-09-15 09:13:44 +0800

    去掉所有与fx相关的东西

--- src/main/java/com/lee/VirtualWmsTaskApplication.java
+++ src/main/java/com/lee/VirtualWmsTaskApplication.java
@@ -1,8 +1,5 @@
 package com.lee;
 
-import com.lee.control.tcp.MainTCPController;
-import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
-import javafx.stage.Stage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,16 +12,7 @@ public class VirtualWmsTaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VirtualWmsTaskApplication.class, args);
-//        launch(VirtualWmsTaskApplication.class, MainTCPController.class, args);
     }
 
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        stage.setTitle("virtual wms");
-//        stage.setOnCloseRequest(event -> {
-//            System.exit(0);
-//        });
-//        super.start(stage);
-//    }
 
 }
--- src/main/java/com/lee/control/http/MainHttpServerController.java
+++
@@ -1,8 +0,0 @@
-package com.lee.control.http;
-
-/**
- * @Date: 2022/12/6 9:05
- * @Description: TODO
- */
-public class MainHttpServerController {
-}
